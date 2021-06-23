ee383f2a9e4953e06c9d07895576fe6115dbd3d4
flytothe <1378902892@qq.com>
2021-06-23 22:14:02 +0800

    '提交'

---
+++ src/test/HashMapTest.java
@@ -0,0 +1,16 @@
+package test;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>(1);
+        
+        map.put("k1", "v1");
+        map.put("k2", "v2");
+    }
+
+}
